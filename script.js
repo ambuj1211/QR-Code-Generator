56e3db2f23329eb1c680c3737f0eb0e29ff44c37
Brilliant Ambuj <76522659+ambuj1211@users.noreply.github.com>
2023-12-27 14:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let inpText = document.getElementById("inpText");
+function generateQR(){
+    
+    qrImage.src =" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+inpText.value;
+
+}
