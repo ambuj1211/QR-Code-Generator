9b591cfb77ca7ad24c6cbf26bcd374cdd22431e6
Brilliant Ambuj <76522659+ambuj1211@users.noreply.github.com>
2023-12-27 14:32:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let inpText = document.getElementById("inpText");
 function generateQR(){
-    
+    imgBox.style.display = "flex";
     qrImage.src =" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+inpText.value;
 
 }
